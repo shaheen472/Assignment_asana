6565510ed3b88a8a91df4aa213ba6fe4f61801e8
shaheen472 <2024psp0109@iitjammu.ac.in>
2026-01-07 06:53:38 +0530

    Create __init__.py

---
+++ src/scrapers/__init__.py
@@ -0,0 +1,5 @@
+from .organization_data import load_organization
+from .team_catalog import load_team_catalog
+from .project_templates import load_project_templates
+from .role_catalog import load_role_distribution, role_to_titles
+from .name_corpus import load_first_names, load_last_names
