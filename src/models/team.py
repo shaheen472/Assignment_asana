1d75729076daeb848b902db686bbe2431f9ccf4f
shaheen472 <2024psp0109@iitjammu.ac.in>
2026-01-07 07:34:24 +0530

    Update team.py

--- src/models/team.py
+++ src/models/team.py
@@ -1,11 +1,16 @@
-from dataclasses import dataclass
-from datetime import datetime
-
-
-@dataclass
 class Team:
-    team_id: str
-    org_id: str
-    name: str
-    description: str
-    created_at: datetime
+    def __init__(
+        self,
+        team_id: str,
+        org_id: str,
+        name: str,
+        description: str,
+        function: str,
+        created_at
+    ):
+        self.team_id = team_id
+        self.org_id = org_id
+        self.name = name
+        self.description = description
+        self.function = function
+        self.created_at = created_at
